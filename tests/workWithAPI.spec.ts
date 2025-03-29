cb024b187ed03308a55f7aa1811bcf28cee85494
vlad <vladsheverda7@gmail.com>
2025-03-29 19:36:15 +0400

    setting up the project + mocking api + modifying API response

---
+++ tests/workWithAPI.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
+
+test.beforeEach(async ({ page }) => {
+    await page.route('*/**/api/tags', async route => {
+        await route.fulfill({
+            body: JSON.stringify(tags),
+        });
+    });
+
+    await page.route('*/**/api/articles*', async route => {
+        const response = await route.fetch();
+        const responseBody = await response.json();
+
+        responseBody.articles[0].title = 'This is a test title';
+        responseBody.articles[0].description = 'This is a test description';
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody),
+        });
+    });
+
+    await page.goto('https://conduit.bondaracademy.com/');
+});
+
+test('has title', async ({ page }) => {
+    const titleLocator = page.locator('.navbar-brand');
+    const tagsList = tags.tags;
+
+    await expect(page.locator('.tag-list')).toContainText(tagsList[0]);
+    await expect(titleLocator).toHaveText('conduit');
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title');
+    await expect(page.locator('app-article-list p').first()).toContainText('This is a test description');
+});
