b89cea46eed40ef8be981a30dd19e279686efacf
vlad <vladsheverda7@gmail.com>
2025-04-01 17:45:49 +0400

    delete article request has been created

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,18 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    timeout: 10000,
+    globalTimeout: 60000,
+    expect: {
+        timeout: 2000,
+    },
     testDir: './tests',
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 2 : 1,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,7 +34,9 @@ export default defineConfig({
         // baseURL: 'http://127.0.0.1:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'on',
+        //actionTimeout: 5000,
+        //navigationTimeout: 5000,
     },
 
     /* Configure projects for major browsers */
@@ -39,15 +46,15 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
 
-        // {
-        //   name: 'firefox',
-        //   use: { ...devices['Desktop Firefox'] },
-        // },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
 
-        // {
-        //   name: 'webkit',
-        //   use: { ...devices['Desktop Safari'] },
-        // },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
 
         /* Test against mobile viewports. */
         // {
